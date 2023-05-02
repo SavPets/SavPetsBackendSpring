5174c9a45eb7eea7da387abf1c56052204af18b4
Pessinaaa <pedro.pessina99@gmail.com>
2023-05-02 20:40:40 -0300

    :bulb: Organização das regras de validação

--- src/main/resources/static/js/validations.js
+++ src/main/resources/static/js/validations.js
@@ -11,15 +11,51 @@ const validator = new JustValidate('.form',
 
 // adicione mais regras para cada campo que for necessário
 const inputsRules = {
+	
+  // REGRAS DE FORMULARIOS GERAIS
   name: [
     { rule: 'required', errorMessage: 'O campo nome é obrigatório' },
     { rule: 'minLength', value: 4, errorMessage: 'O campo nome deve conter no mínimo 4 caracteres' }
   ],
+  cpf: [
+    { rule: 'required', errorMessage: 'O campo CPF é obrigatório' },
+    { rule: 'minLength', value: 14, errorMessage: 'O Campo CPF deve possuir todos os caracteres' },
+    { rule: 'maxLength', value: 14, errorMessage: 'O Campo CPF deve possuir todos os caracteres' },
+  ],
+  firstName: [
+    { rule: 'required', errorMessage: 'O campo de Primeiro Nome é obrigatório' },
+    { rule: 'minLength', value: 2, errorMessage:  'O campo Primeiro Nome deve possuir no mínimo 2 caracteres' }
+  ],
+  lastName: [
+    { rule: 'required', errorMessage: 'O campo de Último Nome é obrigatório' },
+    { rule: 'minLength', value: 2, errorMessage:  'O campo Último Nome deve possuir no mínimo 2 caracteres' }
+  ],
+  cep: [
+    { rule: 'required', errorMessage: 'O campo CEP é obrigatório' },
+    { rule: 'minLength', value: 9, errorMessage: 'O campo CEP deve possuir todos os caracteres' },
+    { rule: 'maxLength', value: 9, errorMessage: 'O campo CEP deve possuir todos os caracteres' }
+  ],
+  rua: [
+    { rule: 'required', errorMessage: 'O campo Endereço é obrigatório' },
+    { rule: 'minLength', value: 5, errorMessage: 'O campo Endereco deve possuir ao menos 5 caracteres'}
+  ],
+  locationNumber: [
+    { rule: 'required', errorMessage: 'O campo Número da Residência é obrigatório' }
+  ],
+  email: [
+    { rule: 'required', errorMessage: 'O campo E-mail é obrigatório' },
+    { rule: 'email', errorMessage: 'O campo E-mail deve possuir um e-mail válido' }
+  ],
+  	
+  // REGRAS DO FORMULARIO DEPARTAMENTO
+ 
   initials: [
     { rule: 'required', errorMessage: 'O campo iniciais é obrigatório' },
     { rule: 'minLength', value: 2, errorMessage: 'O campo iniciais deve conter no mínimo 2 caracteres' },
     { rule: 'maxLength', value: 3, errorMessage: 'O campo iniciais deve conter no máximo 3 caracteres' }
   ],
+  
+  // REGRAS DO FORMULARIO CATEGORIA ANIMAL
   race: [
     { rule: 'required', errorMessage: 'O campo raça é obrigatório' },
     { rule: 'minLength', value: 3, errorMessage: 'O campo raça deve conter no mínimo 3 caracteres' }
@@ -37,8 +73,9 @@ const inputsRules = {
    peltColor: [
     { rule: 'required', errorMessage: 'O campo cor é obrigatório' },
     { rule: 'minLength', value: 4, errorMessage: 'O campo cor deve conter no mínimo 3 caracteres' }
-
   ],
+  
+  // REGRAS DO FORMULARIO MEDICAMENTO
   description: [
  	{ rule: 'required', errorMessage: 'O campo descrição é obrigatório' },
  	{ rule: 'minLength', value: 10, errorMessage: 'O campo descrição deve conter no mínimo 10 caracteres' }
@@ -67,51 +104,38 @@ const inputsRules = {
   manufacturingDate:[
   	{ rule: 'required', errorMessage: 'O campo data de fabricação é obrigatório' }
   ],
-  cpf: [
-    { rule: 'required', errorMessage: 'O campo CPF é obrigatório' },
-    { rule: 'minLength', value: 14, errorMessage: 'O Campo CPF deve possuir todos os caracteres' },
-    { rule: 'maxLength', value: 14, errorMessage: 'O Campo CPF deve possuir todos os caracteres' },
-  ],
-  firstName: [
-    { rule: 'required', errorMessage: 'O campo de Primeiro Nome é obrigatório' },
-    { rule: 'minLength', value: 2, errorMessage:  'O campo Primeiro Nome deve possuir no mínimo 2 caracteres' }
-  ],
-  lastName: [
-    { rule: 'required', errorMessage: 'O campo de Último Nome é obrigatório' },
-    { rule: 'minLength', value: 2, errorMessage:  'O campo Último Nome deve possuir no mínimo 2 caracteres' }
-  ],
-  animalCategory: [
-    { rule: 'required', errorMessage: 'Uma das opções de Categoria de Animal deve ser selecionada' }
-  ],
+  
+  // REGRAS DO FORMULARIO FUNCIONARIO
+  
+  
+  // REGRAS DO FORMULARIO RELATORIO ANIMAL
   local: [
     { rule: 'required', errorMessage: 'O campo Local é obrigatório' },
     { rule: 'minLength', value: 5, errorMessage: 'O campo Local deve possuir no mínimo 5 caracteres'}
   ],
+  
+  // REGRAS DO FORMULARIO CLIENTE
   telefone: [
     { rule: 'required', errorMessage: 'O campo Telefone é obrigatório' },
     { rule: 'minLength', value: 14, errorMessage: 'O campo Telefone deve possuir todos caracteres'},
     { rule: 'maxLength', value: 14, errorMessage: 'O campo Telefone deve possuir todos caracteres'}
   ],
-  cep: [
-    { rule: 'required', errorMessage: 'O campo CEP é obrigatório' },
-    { rule: 'minLength', value: 9, errorMessage: 'O campo CEP deve possuir todos os caracteres' },
-    { rule: 'maxLength', value: 9, errorMessage: 'O campo CEP deve possuir todos os caracteres' }
-  ],
-  rua: [
-    { rule: 'required', errorMessage: 'O campo Endereço é obrigatório' },
-    { rule: 'minLength', value: 5, errorMessage: 'O campo Endereco deve possuir ao menos 5 caracteres'}
-  ],
-  locationNumber: [
-    { rule: 'required', errorMessage: 'O campo Número da Residência é obrigatório' }
-  ],
-  email: [
-    { rule: 'required', errorMessage: 'O campo E-mail é obrigatório' },
-    { rule: 'email', errorMessage: 'O campo E-mail deve possuir um e-mail válido' }
-  ],
   surname: [
     { rule: 'required', errorMessage: 'O campo Sobrenome é obrigatório' },
     { rule: 'minLength', value: 3, errorMessage: 'O campo Sobrenome deve possuir no mínimo 3 caracteres' }
   ],
+  
+  // REGRAS DO FORMULARIO CADASTRO
+  password: [
+	{ rule: 'required', errorMessage: 'O campo senha é obrigatório' },
+	{ rule: 'strongPassword', errorMessage: 'A senha deve conter pelo menos 8 dígitos, uma letra maiúscula, uma minúscula, um caractere especial e um número' }
+  ],
+  repeatPassword: [
+	{ rule: 'required', errorMessage: 'O campo repetir senha é obrigatório' },
+	{ rule: 'strongPassword', errorMessage: 'A senha deve conter pelo menos 8 dígitos, uma letra maiúscula, uma minúscula, um caractere especial e um número' }
+  ],
+  
+  // REGRAS DO FORMULARIO FORNECEDOR
   provider: [
     { rule: 'required', errorMessage: 'O campo Fornecedor é obrigatório' }
   ],
@@ -119,14 +143,6 @@ const inputsRules = {
     { rule: 'required', errorMessage: 'O campo CNPJ é obrigatório' },
     { rule: 'minLength', value: 18, errorMessage: 'O campo CNPJ deve possuir todos os caracteres' },
     { rule: 'maxLength', value: 18, errorMessage: 'O campo CNPJ deve possuir todos os caracteres' }
-  ],
-  password: [
-	{ rule: 'required', errorMessage: 'O campo senha é obrigatório' },
-	{ rule: 'strongPassword', errorMessage: 'A senha deve conter pelo menos 8 dígitos, uma letra maiúscula, uma minúscula, um caractere especial e um número' }
-  ],
-  repeatPassword: [
-	{ rule: 'required', errorMessage: 'O campo senha é obrigatório' },
-	{ rule: 'strongPassword', errorMessage: 'A senha deve conter pelo menos 8 dígitos, uma letra maiúscula, uma minúscula, um caractere especial e um número' }
   ]
 }
 
