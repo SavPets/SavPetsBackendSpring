b0cc1326106bfea34926336d3749677817397c12
mateussantanasilva <santanasilva1778@gmail.com>
2023-05-14 21:17:12 -0300

    🍱 Separando conteúdo main que estava na home.js

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,48 +1,3 @@
-// =============== MENU ===============
-// Elementos para manipular durante a ação do menu
-const burger = document.querySelector('.burger')
-const menuMobile = document.querySelector('.menu-mobile')
-const containerWelcome = document.querySelector('.presentation-container_welcome')
-const containerMain = document.querySelector('.container-main_content')
-const containerChoose = document.querySelector('.identify-container_choose')
-const form = document.querySelector('.form')
-
-const vlibrasWidget = document.querySelector('.vlibras-widget')
-
-burger.addEventListener('click', () => openMobileMenu(burger))
-
-function openMobileMenu(burger) {
-
-    // Remove conjunto de estilos que o scroll reveal insere
-    if (containerWelcome) {
-        containerWelcome.setAttribute('style', '')
-    }
-
-    if (containerMain) {
-        containerMain.setAttribute('style', '')
-    }
-
-    if (form) {
-        form.classList.toggle('layerdown')
-    }
-
-    // Coloca o conteúdo uma camada abaixo do menu
-    if (containerChoose) {
-        const burgerIsOpen = burger.classList.contains('open')
-        if (burgerIsOpen) {
-            setTimeout(() => containerChoose.classList.remove('layerdown'), 400)
-        } else {
-            containerChoose.classList.add('layerdown')
-        }
-    }
-
-    vlibrasWidget.classList.toggle('to-hide')
-
-    burger.classList.toggle('open')
-    menuMobile.classList.toggle('active-menu-mobile')
-    bodyElement.classList.toggle('active-menu-mobile')
-}
-
 // =============== BACK TO TOP ===============
 const backToTop = document.querySelector('.back-to-top')
 const scrollDown = document.querySelector('.scroll-down')
@@ -58,13 +13,6 @@ window.addEventListener('scroll', () => {
 })
 
 // =============== SCROLL REVEAL SETTINGS ===============
-const scrollReveal = ScrollReveal({
-    origin: 'left',
-    distance: '40px',
-    duration: '800',
-    delay: 150
-})
-
 scrollReveal.reveal(`
     .presentation-container_welcome,
     .optionshelp-container_title,
@@ -75,8 +23,3 @@ scrollReveal.reveal(`
     .start-container,
     .contact-container
 `)
-
-scrollReveal.reveal(`
-    .container-main_content,
-    .footer-container
-`, { origin: 'top' })
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -35,3 +35,62 @@ function changeColorMenuOnScroll(menu, accessibilityBar) {
     menu.classList.add('fixed-menu')
     accessibilityBar.classList.add('to-hide')
 }
+
+// =============== MENU MOBILE ===============
+// Elementos para manipular durante a ação do menu mobile
+const burger = document.querySelector('.burger')
+const menuMobile = document.querySelector('.menu-mobile')
+const containerMain = document.querySelector('.container-main_content')
+const form = document.querySelector('.form')
+const vlibrasWidget = document.querySelector('.vlibras-widget')
+
+// Aparecem na home
+const containerWelcome = document.querySelector('.presentation-container_welcome')
+const containerChoose = document.querySelector('.identify-container_choose')
+
+burger.addEventListener('click', () => openMobileMenu(burger))
+
+function openMobileMenu(burger) {
+
+    // Remove conjunto de estilos que o scroll reveal insere
+    if (containerWelcome) {
+        containerWelcome.setAttribute('style', '')
+    }
+
+    if (containerMain) {
+        containerMain.setAttribute('style', '')
+    }
+
+    if (form) {
+        form.classList.toggle('layerdown')
+    }
+
+    // Coloca o conteúdo uma camada abaixo do menu
+    if (containerChoose) {
+        const burgerIsOpen = burger.classList.contains('open')
+        if (burgerIsOpen) {
+            setTimeout(() => containerChoose.classList.remove('layerdown'), 400)
+        } else {
+            containerChoose.classList.add('layerdown')
+        }
+    }
+
+    vlibrasWidget.classList.toggle('to-hide')
+
+    burger.classList.toggle('open')
+    menuMobile.classList.toggle('active-menu-mobile')
+    bodyElement.classList.toggle('active-menu-mobile')
+}
+
+// =============== SCROLL REVEAL SETTINGS ===============
+const scrollReveal = ScrollReveal({
+    origin: 'left',
+    distance: '40px',
+    duration: '800',
+    delay: 150
+})
+
+scrollReveal.reveal(`
+    .container-main_content,
+    .footer-container
+`, { origin: 'top' })
