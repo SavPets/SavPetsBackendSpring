becae1631fb0b4982183b8dde860febd4f099087
guilhermegg8 <guillhermegoncalvesf12@gmail.com>
2023-05-15 21:53:43 -0300

    :sparkles: Adicionando registro de departamento, medicamento e fornecedor no LoadDatabase

--- src/main/java/com/fatec/scc/services/LoadDatabase.java
+++ src/main/java/com/fatec/scc/services/LoadDatabase.java
@@ -4,10 +4,15 @@ import com.fatec.scc.model.adoptionCampaign.AdoptionCampaign;
 import com.fatec.scc.model.adoptionCampaign.MaintainAdoptionCampaignRepository;
 import com.fatec.scc.model.client.Client;
 import com.fatec.scc.model.client.MaintainClientRepository;
+import com.fatec.scc.model.departament.Departament;
+import com.fatec.scc.model.departament.MaintainDepartamentRepository;
 import com.fatec.scc.model.employee.Employee;
 import com.fatec.scc.model.employee.MaintainEmployeeRepository;
+import com.fatec.scc.model.medicine.MaintainMedicineRepository;
+import com.fatec.scc.model.medicine.Medicine;
 import com.fatec.scc.model.occupation.MaintainOccupationRepository;
 import com.fatec.scc.model.occupation.Occupation;
+import com.fatec.scc.model.provider.Provider;
 import com.fatec.scc.services.occupation.MaintainOccupation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,7 +30,7 @@ class LoadDatabase {
 //	@SuppressWarnings("unused")
 	private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 	@Bean
-	CommandLineRunner initDatabase(MaintainAdoptionCampaignRepository maintainAdoptionCampaignRepository, MaintainEmployeeRepository maintainEmployeeRepository, MaintainOccupationRepository maintainOccupationRepository, MaintainClientRepository maintainClientRepository, MaintainProviderRepository maintainProviderRepository) {
+	CommandLineRunner initDatabase(MaintainAdoptionCampaignRepository maintainAdoptionCampaignRepository, MaintainEmployeeRepository maintainEmployeeRepository, MaintainOccupationRepository maintainOccupationRepository, MaintainClientRepository maintainClientRepository, MaintainProviderRepository maintainProviderRepository, MaintainMedicineRepository maintainMedicineRepository, MaintainDepartamentRepository maintainDepartamentRepository) {
 		return args -> {
 			maintainAdoptionCampaignRepository.deleteAll();
 			AdoptionCampaign adoptionCampaign = new AdoptionCampaign("Patas em Busca de Lar", "Em nosso evento, você terá a oportunidade de conhecer cães e gatos resgatados, prontos para encontrar um lar amoroso e oferecer carinho, lealdade e momentos de felicidade.", "2023-05-15", "07:00", "21:00", "Parque Ibirapuera");
@@ -43,8 +48,19 @@ class LoadDatabase {
 			Client client = new Client("Sued", "Santos", "54906578910", "11987654321", "03807420", "Rua dos Bobos", 0, "Casa");
 			maintainClientRepository.save(client);
 
+			maintainProviderRepository.deleteAll();
+			Provider provider = new Provider("PetShop", "12345678910111", "03807420", "Rua Jacaré", 0, "Casa");
+			maintainProviderRepository.save(provider);
+
+			maintainMedicineRepository.deleteAll();
+			Medicine medicine = new Medicine("PetShop", "Vacina", "bula exemplo", "Vacina para raiva", "2023-05-15", "Vacina para raiva", "2023-05-15", 10, "2023-05-15");
+			maintainMedicineRepository.save(medicine);
+
+			maintainDepartamentRepository.deleteAll();
+			Departament departament = new Departament("Informatica", "TI");
+			maintainDepartamentRepository.save(departament);
 
 
-		};
+			};
 	}
 }
